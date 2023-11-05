b13bcb6b08a8ab6ba91125d55b59bb9434997ae5
Mohamed Aziz Charrada <mohamedaziz.charrada@esprit.tn>
2023-11-05 20:06:37 +0100

    Fixing

--- src/main/java/tn/esprit/spring/khaddem/entities/Option.java
+++ src/main/java/tn/esprit/spring/khaddem/entities/Option.java
@@ -1,5 +1,5 @@
 package tn.esprit.spring.khaddem.entities;
 
-public enum Niveau {
-    JUNIOR,SENIOR,EXPERT
+public enum Option {
+    GAMIX,SE,SAE,INFINI
 }
--- src/main/java/tn/esprit/spring/khaddem/entities/Universite.java
+++ src/main/java/tn/esprit/spring/khaddem/entities/Universite.java
@@ -1,6 +1,5 @@
 package tn.esprit.spring.khaddem.entities;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,20 +8,20 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class DetailEquipe implements Serializable {
+public class Universite implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idDetailEquipe;
-    private Integer salle;
-    private String thematique;
-    @OneToOne(mappedBy = "detailEquipe")
-    @JsonIgnore
-    private  Equipe equipe;
+    private Integer idUniversite;
+    private String nomUniv;
+   // @OneToMany(cascade = CascadeType.ALL)
+  //  @JsonIgnore
+    //private List<Departement>departements;
 }
